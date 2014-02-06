4c86f539e091ce34abc7315e35de022928cc8281
lham <linus.hammarlund@gmail.com>
2014-02-06 11:42:40 +0100

    A simple AI which places randomly

---
+++ assignment1/SimpleAI.py
@@ -0,0 +1,18 @@
+import copy
+from random import choice
+
+# SimpleAI places
+class SimpleAI(object):
+  def __init__(self):
+    pass
+
+  def getMove(self, board, color):
+    self.board = copy.deepcopy(board)
+    self.moves = []
+
+    for y in xrange(8):
+      for x in xrange(8):
+        if board.isLegal(y, x, color):
+          self.moves.append(y, x)
+
+    return choice(moves)
