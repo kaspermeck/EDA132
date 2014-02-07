757aa89b7f49a2a5eb22320da7f6048e8c6ce493
lham <linus.hammarlund@gmail.com>
2014-02-07 12:31:46 +0100

    working minimax ai

--- assignment1/MinimaxAI.py
+++ assignment1/MinimaxAI.py
@@ -1,4 +1,5 @@
 import copy
+from Board import *
 
 # SimpleAI places
 class MinimaxAI(object):
@@ -7,7 +8,7 @@ class MinimaxAI(object):
     self.aicolor = color
 
   def getMove(self, board, color):
-    return getScoreMove(board, color, 0, 3)
+    return self.getScoreMove(board, color, 0, 3)[0]
 
   def getScoreMove(self, board, color, depth, maxDepth):
     moves = []
@@ -15,24 +16,33 @@ class MinimaxAI(object):
     for y in xrange(8):
       for x in xrange(8):
         if board.isLegal(y, x, color):
-          moves.append(((y, x), score(y, x, color)))
+          moves.append([(y, x), self.score(board, y, x, color)])
 
     if depth < maxDepth:
-      for i in moves:
+      for move in moves:
         b = copy.deepcopy(board)
-        b.place(*moves[i][0], color])
-        score = getScoreMove(b, Board.oppositeColor(color), depth + 1, maxDepth)
-      
-        if color == aicolor: # next level is opponent
-          moves[i][1] -= score 
-        else:
-          moves[i][1] += score
+        b.place(move[0][0], move[0][1], color)
+        score = self.getScoreMove(b, Board.oppositeColor(color), depth + 1, maxDepth)
 
-    return sorted(moves, key = lambda move: move[1])[-1]
+        if not score: # opponent can make no move, good? (note negation)
+          score = -1000  
+        else:
+          score = score[1]
+      
+        if color == self.aicolor: # next level is opponent, therefor minus
+          move[1] -= score 
+        else:
+          move[1] += score
+
+    # Sort by score, return last element (highest score)
+    if not moves:
+      return []
+    else:
+      return sorted(moves, key = lambda move: move[1])[-1]
 
     
 
-  def score(self, y, x, color):
+  def score(self, board, y, x, color):
     corners = ((0,0),(7,7),(0,7),(7,0))
     next_to_corners = (
       (0,1),(1,0),(1,1),
@@ -46,6 +56,6 @@ class MinimaxAI(object):
     if (y,x) in corners:
       score += 20
     if (y,x) in next_to_corners:
-      score -= 10)
+      score -= 10
 
     return score
