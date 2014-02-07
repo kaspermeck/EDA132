d484e49cea10faeecbf84c6f7eb632d09aeb354e
lham <linus.hammarlund@gmail.com>
2014-02-07 12:58:10 +0100

    Scoreboard heuristics

--- assignment1/MinimaxAI.py
+++ assignment1/MinimaxAI.py
@@ -51,11 +51,18 @@ class MinimaxAI(object):
       (6,0),(7,1),(6,1)
     )
 
+    scoreboard = [
+      [99, - 8,  8,  6,  6,  8, - 8, 99],
+      [-8, -24, -4, -3, -3, -4, -24, -8],
+      [ 8, - 4,  7,  4,  4,  7, - 4,  8],
+      [ 6, - 3,  4,  0,  0,  4, - 3,  6],
+      [ 6, - 3,  4,  0,  0,  4, - 3,  6],
+      [ 8, - 4,  7,  4,  4,  7, - 4,  8],
+      [-8, -24, -4, -3, -3, -4, -24, -8],
+      [99, - 8,  8,  6,  6,  8, - 8, 99]
+    ]
+
     score = board.scorediff(y, x, color)
-    
-    if (y,x) in corners:
-      score += 20
-    if (y,x) in next_to_corners:
-      score -= 10
+    score += scoreboard[y][x]
 
     return score
