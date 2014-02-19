e49c8890a514c70484baa5d659fa161bd7c0cb58
lham <linus.hammarlund@gmail.com>
2014-02-19 16:42:54 +0100

    Fixed a yes-list and dded some comments

--- assignment2/Importance.py
+++ assignment2/Importance.py
@@ -1,5 +1,6 @@
 from __future__ import division
 from math import log
+
 # Example dataset
 ex_dataset = {'relation':   "weather", 
               'attributes': [("outlook", ("sunny", "rainy", "foggy")),
@@ -13,30 +14,36 @@ ex_dataset = {'relation':   "weather",
 
 # Return a gain value for a given attribute in the given dataset
 def importance(dataset, attr):
-  # Compute the gain of the attribute
   gain = False
   size = len(dataset['data'])
+  yes_values = [True, 1, "yes", "ja", "true", "TRUE", "True"]
 
   for attr_pos, (attr_name, attr_values) in enumerate(dataset['attributes']):
+    # Check if the current attribute is the one we want to compute gain for
     if attr != attr_name:
       continue
 
+    # Compute gain
     gain = 1.0
-    for attr_value in attr_values:
-      pos = 0
-      neg = 0
+    for attr_value in attr_values: 
+      pos = neg = 0
       for data in dataset['data']:
         if data[attr_pos] == attr_value:
-          if data[-1]:
+          # Assign the boolean value from the last column as pos or neg
+          if data[-1] in yes_values:
             pos += 1
           else:
             neg += 1
+      # Subtract the gain if we had any attr_value assigned in the dataset
       if pos+neg > 0:
-        gain -= (pos+neg)/size * Bfun(pos/(pos+neg))
+        gain -= (pos+neg)/size * Bfunc(pos/(pos+neg))
 
+  # gain will be False it attr was not found in the dataser,
+  # otherwise it will be the gain.
   return gain
 
-def Bfun(q):
+def Bfunc(q):
+  # val = -(q*log(q) + (1-q)*log(1-q))
   val = 0
   if q > 0:
     val += q*log(q, 2)
